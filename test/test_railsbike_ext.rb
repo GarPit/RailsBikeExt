476be72dffd81b24b0f16a054aaeda5f35ac05c5
Alexander Kirillov <saratovsource@gmail.com>
2011-07-13 11:03:43 +0400

    Initial commit to railsbike_ext.

---
+++ test/test_railsbike_ext.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestRailsbikeExt < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
