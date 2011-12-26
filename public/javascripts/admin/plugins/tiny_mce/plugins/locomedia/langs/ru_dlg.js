3ac302a1f0e65e9ee0c8df2e493bd30c9e04ca42
Kirillov Alexander <saratovsource@gmail.com>
2011-12-26 14:55:23 +0300

    Русский перевод для диалога медиа-файлов

---
+++ public/javascripts/admin/plugins/tiny_mce/plugins/locomedia/langs/ru_dlg.js
@@ -0,0 +1,9 @@
+tinyMCE.addI18n('ru.locomedia_dlg',{
+  dialog_title: 'Вставить медиа',
+  upload: 'Загрузить медиа',
+  loading: 'Загрузка...',
+  uploading: 'Загрузка файла...',
+  destroying: 'Удаление...',
+  confirm: 'Вы уверены?',
+  no_items: 'Пока нет никаких файлов.'
+});
